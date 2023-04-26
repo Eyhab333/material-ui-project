d70d53e338cb46d86b62d0dbaa3f76998adfef71
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-04-26 20:58:14 +0300

    first commit

---
+++ src/pages/Root.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+  return (
+    <div>
+      <header>
+        my header
+      </header>
+
+      <Outlet />
+    </div>
+  );
+}
+
+export default Root;
---
+++ src/pages/create/Create.jsx
@@ -0,0 +1,12 @@
+import './Create.css'
+
+
+const Create = () => {
+  return (
+    <div>
+      createeeeeeeeeeeee
+    </div>
+  );
+}
+
+export default Create;
