c39dab591a3f08921440d9b78633a4ecded2daa7
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-05-10 14:53:21 +0300

    React Hook Form

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -3,15 +3,35 @@ import "./Create.css";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [cost, setCost] = useState(0);
   const navigate = useNavigate();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {
+
+    fetch(" http://localhost:3000/mydata  ", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, cost }),
+  }).then(() => {
+    navigate("/");
+  });
+  }
+
   return (
     <Box
       component="form"
+      onSubmit={handleSubmit(onSubmit)}
       sx={{
         p: "50px",
         border: "0.1px solid white",
@@ -23,6 +43,8 @@ const Create = () => {
       }}
     >
       <TextField
+        {...register("title", { required: {value: true,  message: "Title is required"}, minLength: 
+        {value: 3, message: "Title must be at least 3 characters"}, maxLength: {value: 20, message: "Title must be at most 20 characters"} })}
         onChange={(e) => setTitle(e.target.value)}
         InputProps={{
           startAdornment: <InputAdornment position="start">T</InputAdornment>,
@@ -31,9 +53,12 @@ const Create = () => {
         label="Title"
         variant="outlined"
         autoComplete="off"
+        error={errors.title ? true : false}
+        helperText={errors.title ? errors.title.message : ""}
       />
 
       <TextField
+        {...register("cost", { required: true })}
         onChange={(e) => setCost(Number(e.target.value))}
         InputProps={{
           startAdornment: <InputAdornment position="start">$</InputAdornment>,
@@ -41,24 +66,16 @@ const Create = () => {
         id="outlined-basic"
         label="Cost"
         variant="outlined"
-        autoComplete="none"
+        autoComplete="off"
+        type="number"
+        error={errors.cost ? true : false}
+        helperText={errors.cost ? "Cost is required" : ""}
       />
       <Button
-        onClick={() => {
-          fetch(" http://localhost:3000/mydata  ", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ title, cost }),
-          })
-          .then(() => {
-            navigate('/')
-          })
-        }}
         variant="contained"
         color="success"
         endIcon={<AddCircleIcon />}
+        type="submit"
       >
         Create
       </Button>
